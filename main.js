fdac5aaa0f0b0ef604f114692cbafbf809b9aebf
socratone <gim2origin@gmail.com>
2020-08-30 18:47:53 +0900

    코드 정리, 빌드 정리

---
+++ main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  require('./server');
+
+  // Create the browser window.
+  win = new BrowserWindow({ width: 800, height: 600 });
+
+  // and load the index.html of the app.
+  win.loadURL('http://localhost:8080/');
+}
+
+app.on('ready', createWindow);
